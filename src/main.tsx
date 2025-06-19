e2b702657bed389122f293b5973e0d3f87ab9622
Iashaking <IASHA@HAWAII.EDU>
2025-06-18 21:40:00 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+console.log("🔥 main.tsx loaded at", new Date());
+
 createRoot(document.getElementById("root")!).render(<App />);
